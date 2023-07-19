a7b82f000cf6196f013420db58576fc230a068a7
Sadeem <sadeem@csedge.org>
2023-07-19 10:00:00 -0400

    made a simple outline

---
+++ CYOA_Outline.js
@@ -0,0 +1,69 @@
+//This is the starter file for CYOA
+//Use it to develop your skills as needed
+
+
+//an empty string to hold our user inputs
+//the game loop will depend on this being true
+//to keep track of where we are
+//push any new rooms you create to this array
+
+//******** DEFINE CLASSES *******************
+// Create a class for Room and for Player
+
+//**********INSTANTIATE OBJECTS ***************
+// Create your player
+
+// Create some rooms
+
+
+//add the rooms to the rooms array
+
+//************* START GAME *************************
+function start() {
+
+    // saves the player name
+    console.log("Welcome to The Demo!!");
+    
+    //begin at the bedroom
+
+    // try to theme your game to your setting
+
+    // your game loop
+    
+}
+
+function promptUser() {
+
+}
+
+function checkAnswer(input) {
+
+
+
+    //GO
+    if (inputMsg.includes("go")) {
+
+
+        //LOOK          
+    } else if (inputMsg.includes("look")) {
+        
+
+        //TAKE
+    } else if (inputMsg.includes("take")) {
+
+
+        //Name
+    } else if (inputMsg.includes("name")) {
+
+    } else if (inputMsg.includes("help")) {
+
+
+    } else if (inputMsg == "exit") {
+
+    } else {
+
+    }
+
+}
+
+// start();
---
+++ Jeopardy.js
@@ -0,0 +1,52 @@
+// nested ifs
+const question1 = (age) => {
+
+
+
+};
+question1(5);
+
+// concatenation 
+const question2 = (num) => {
+
+    console.log(num + 5);
+
+};
+question2("3");
+
+// nested loops
+const question3 = () => {
+    let count = 0;
+    for (let i = 0; i < 5; i++){
+        for(let j = 0; j < 2; j++){
+            count++;
+            console.log(`count: ${count} | i: ${i} | j: ${j}`);
+        }
+    }
+
+};
+question3();
+
+// arrays
+const question4 = () => {
+    let food = []
+    
+
+};
+question4();
+
+
+// scope
+const question5 = () => {
+    let food = []
+    
+
+};
+question5();
+
+
+
+// for(let x = 0; x <= 10; x++){
+//     console.log(`${x}: ${9 > x && 6 <= x && x != 5}`)
+// }
+
